93b7450854c84cd805bd0b632efde95010a3e631
Mathusan <mathusanadded@gmail.com>
2022-10-13 00:36:01 +0530

    added controllers

---
+++ src/api/controllers/userController.js
@@ -0,0 +1,34 @@
+const userService = require('../../services/userService')
+
+
+const userController = {
+    registerUser : async (req,res,next) =>{
+        try {
+            const {name,email,password} = req.body
+            const {data} = await userService.signUp({name,email,password})
+            return res.json(data)
+        } catch (error) {
+            next(err)
+        }
+       
+
+    },
+    loginUser  : async (req,res,next) =>{
+        try {
+            const {email,password} = req.body
+            const {data} = await userService.login({email,password})
+            return res.json(data)
+        } catch (error) {
+            next(err)
+        }
+    },
+    getUsers :(req,res,next) =>{
+
+    },
+    updateUser :(req,res,next) =>{
+
+    },
+    deleteUser :(req,res,next) =>{
+
+    },
+}
